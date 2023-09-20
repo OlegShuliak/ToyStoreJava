2ce109472d686acb84d1908a8a65f52cb533c94e
Oleg  Shuliak <nformals@gmail.com>
2023-09-20 03:51:23 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        ToysVendingMachine machine = new ToysVendingMachine(new PriorityQueue<>());
+
+        System.out.println("Введите количество видов игрушек, которое вы хотите поместить в автомат:");
+        int qtyToys = Integer.parseInt(scanner.nextLine());
+
+        File toysBefore = new File("D:\\GeekBrains\\ToyStoreJava\\src\\ToysBeforeGetting.txt");
+        FileWriter start = new FileWriter(toysBefore, true);
+
+        List<String[]> infoToys = insertInfo(qtyToys);
+        machine.PutInMachine(infoToys);
+        for (Toy toy: machine.getToys()) {
+            start.write(toy.toString());
+        }
+        start.flush();
+
+        File prise = new File("D:\\GeekBrains\\ToyStoreJava\\src\\YourPrise.txt");
+        FileWriter play = new FileWriter(prise, true);
+
+        int count = 10;
+
+        do {
+            play.write(machine.GetFromMachine().toString());
+            count--;
+        } while (count > 0);
+        play.flush();
+
+        File toysAfter = new File("D:\\GeekBrains\\ToyStoreJava\\src\\ToysAfterGetting.txt");
+        FileWriter finish = new FileWriter(toysAfter, true);
+        for (Toy toy: machine.getToys()) {
+            finish.write(toy.toString());
+        }
+        finish.flush();
+    }
+
+    public static List<String[]> insertInfo (int qtyToys){
+        List<String[]> infoToys = new ArrayList<>(qtyToys);
+
+        for (int i = 0; i < qtyToys; i++) {
+            System.out.printf("Введите данные %d-й игрушки через пробел (Наименование 'вес'):\n", i + 1);
+            System.out.println("(вес учитывает количество одного вида игрушек в % от вметительности автомата)");
+            do {
+                String[] info = (String.format("%d ", i + 1) + scanner.nextLine()).split(" ");
+                try {
+                    Toy toy = new Toy().toyFromString(info);
+                    infoToys.add(info);
+                    break;
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Неверно введены данные игрушки, повторите попытку ввода:");
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Неверно введен вес игрушки, вес должен быть 0<вес<100, повторите попытку ввода:");
+                }
+            } while (true);
+        }
+        return infoToys;
     }
 }
---
+++ src/Toy.java
@@ -0,0 +1,56 @@
+
+import java.util.Random;
+
+public class Toy implements Comparable<Toy> {
+    private int id;
+    private String name;
+    private int weight;
+
+    public Toy(int id, String name, int weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = new Random().nextInt(100 - weight);
+    }
+
+    public Toy() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Toy toyFromString(String[] info) {
+        Toy toy = new Toy(Integer.parseInt(info[0]), info[1], Math.abs(Integer.parseInt(info[2])));
+        return toy;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id = %d: %s\n", id, name);
+    }
+
+    @Override
+    public int compareTo(Toy o) {
+        return Integer.compare(this.weight, o.weight);
+    }
+}
